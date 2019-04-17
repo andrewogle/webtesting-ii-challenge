4faee1533e005f92a3e2068dfe88c1179f81a334
Andrew <andrewryanogle@gmail.com>
2019-04-16 21:31:18 -0700

    finished dashboard component and tests

---
+++ client/src/components/dashboard/dashboard.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {render, fireEvent, cleanup} from 'react-testing-library';
+import 'jest-dom/extend-expect';
+
+import './Dashboard.js';
+import Dashboard from './Dashboard.js';
+
+describe('The Dashboard component',()=>{
+    it('renders',()=>{
+        render(<Dashboard/>)
+    })
+    it('should display strike button', ()=>{
+        const {getByText} = render(<Dashboard/>)
+        getByText(/strike/i);
+    })
+    it('should display ball button', ()=>{
+        const {getByText} = render(<Dashboard/>)
+        getByText(/ball/i);
+    })
+    it('should display foul button', ()=>{
+        const {getByText} = render(<Dashboard/>)
+        getByText(/foul/i);
+    })
+    it('should display hit button', ()=>{
+        const {getByText} = render(<Dashboard/>)
+        getByText(/hit/i);
+    })
+})
